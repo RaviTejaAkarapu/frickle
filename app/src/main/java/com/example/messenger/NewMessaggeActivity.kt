1af39f9d145501990198df8a4472e8ff0e43a161
Raviteja-a <araviteja118@gmail.com>
2019-05-15 13:01:35 +0530

    migrated to androidx

--- app/src/main/java/com/example/messenger/NewMessaggeActivity.kt
+++ app/src/main/java/com/example/messenger/NewMessaggeActivity.kt
@@ -1,7 +1,7 @@
 package com.example.messenger
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.xwray.groupie.GroupAdapter
 import com.xwray.groupie.Item
 import com.xwray.groupie.ViewHolder
